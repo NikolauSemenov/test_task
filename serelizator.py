fc05b478b10bd05ef095c840fb550ce0db1273da
Xybyyn <58708717+NikolauSemenov@users.noreply.github.com>
2021-06-22 21:48:28 +0500

    Add files via upload

--- serelizator.py
+++ serelizator.py
@@ -7,20 +7,27 @@ class UserSchema(Schema):
     id = fields.UUID(dump_only=True)
 
     name = fields.Str(
+        required=True,
         error_messages={
             'invalid': 'Поле name должно быть строкой'})
 
     last_name = fields.Str(
+        required=True,
         error_messages={
             'invalid': 'Поле last_name должно быть строкой'})
 
     password = fields.Str(
+        required=True,
         error_messages={
             'invalid': 'Поле password должно быть строкой'})
 
     birthday = fields.Str(
+        required=True,
         error_messages={
             'invalid': 'Поле birthday должно быть строкой'})
 
-    login = fields.Str()
-    admin_flag = fields.Bool()
+    login = fields.Str(
+        required=True,
+        error_messages={
+            'invalid': 'Поле login должно быть строкой'})
+    admin_flag = fields.Bool(required=True)
